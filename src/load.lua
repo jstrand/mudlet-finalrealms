65f3ab2a7f7b5e7d8044567b0c05fcdc0a7ddea2
jstrand <jstrand@github.com>
2021-12-22 16:53:56 +0100

    Use weights instead of number of steps to take next xp step

--- src/load.lua
+++ src/load.lua
@@ -11,6 +11,16 @@ for i, file in ipairs(files) do dofile(frModulePath() .. "/src/" .. file) end
 
 function FR.setup()
     -- Init code
+    -- TODO: setupContainers
+    leftOfMapContainer = Geyser.Container:new({
+        name = "leftOfMapContainer",
+        x = 0,
+        y = 0,
+        width = "70%",
+        height = "100%"
+    })
+    leftOfMapContainer:flash()
+
     setupButtons()
     FR.setupDps()
 end
